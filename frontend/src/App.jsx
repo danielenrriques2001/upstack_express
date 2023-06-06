6b58ffed4d82a09effac7b8940aee53b7dc882b6
daniel <daniel.enrriquesmercedes@gmail.com>
2023-06-06 15:12:23 +0200

    FT: i created the fronted files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import '../src/index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-red-700 font-mono'>hallo</h1>
+    </>
+  )
+}
+
+export default App
