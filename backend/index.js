1593640adde0bfb5af5ee9b938e87537282e6a88
daniel <daniel.enrriquesmercedes@gmail.com>
2023-06-07 16:32:14 +0200

    ft: i set te cors config

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import ConnectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import projectRoutes from "./routes/projectRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
+import cors from 'cors';
 
 const app = express();
 app.use(express.json())
@@ -12,6 +13,20 @@ const PORT = process.env.PORT_URI || 4000 ;
 dotenv.config();    
 
 ConnectDB();
+
+const whiteList = [process.env.FRONTED_URL];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if(whiteList.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error('Error Cors'))
+        }
+    }
+}
+
+app.use(cors(corsOptions))
 
 //Routing
 app.use('/api/users', userRoutes );
